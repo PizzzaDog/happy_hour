75d594b5dda336ba6abdb238f4dc2eb03805af84
Nikolai Bogdan <nbogdan@spribe.io>
2023-09-22 16:56:42 +0200

    Initial commit

---
+++ src/main/java/co/spribe/controller/CoctailController.java
@@ -0,0 +1,8 @@
+package co.spribe.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CoctailController {
+
+}
---
+++ src/main/java/co/spribe/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package co.spribe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String check() {
+        return "I'm alive";
+    }
+}

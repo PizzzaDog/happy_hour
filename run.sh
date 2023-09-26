f1186ada3db22e7fb4c12ffd22e63c7415f148fe
Nikolai Bogdan <nbogdan@spribe.io>
2023-09-26 14:25:50 +0200

    optimize maven dependencies

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker stop happy || true
-docker rm happy || true
+docker stop happy_con || true
+docker rm happy_con || true
 docker build -t happy .
 docker run -d -p 8091:8091 --name happy_con happy
--- src/main/java/co/spribe/controller/CocktailController.java
+++ src/main/java/co/spribe/controller/CocktailController.java
@@ -2,14 +2,13 @@ package co.spribe.controller;
 
 import co.spribe.model.Cocktail;
 import co.spribe.service.CocktailService;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class CoctailController {
+public class CocktailController {
     @Autowired
     CocktailService cocktailService;
 
